176d9b2db27e08d489c16daff9cc9e63b7a05b13
unknown <alexboyuchen@gmail.com>
2020-10-23 14:43:08 -0700

    added banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Header from './components/header';
+import Banner from './components/banner';
 import './App.css';
 
 class App extends Component {
   render(){
   return (
+    <div>
     <Header></Header>
+    <Banner></Banner>
+    </div>
   );
   }
 }
